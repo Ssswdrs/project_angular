fc11b4d1b019e6f950089bc43157428c5373a08e
Ssswdrs <saksornsawatdee@gmail.com>
2024-06-28 11:51:17 +0700

    add component

--- Store/src/app/app.module.ts
+++ Store/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalComponent } from './components/personal/personal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalComponent
   ],
   imports: [
     BrowserModule,
