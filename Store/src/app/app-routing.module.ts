51496bc67ee2949e8f4e9a10b55d98c2d0d80751
Ssswdrs <saksornsawatdee@gmail.com>
2024-06-28 12:06:39 +0700

    add path

--- Store/src/app/app-routing.module.ts
+++ Store/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { PersonalComponent } from 'src/app/components/personal/personal.component'
+const routes: Routes = [
+  {path: 'personal' , component : PersonalComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- Store/src/app/app.module.ts
+++ Store/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalComponent } from './components/personal/personal.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalComponent
+    PersonalComponent,
+    HeaderComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
